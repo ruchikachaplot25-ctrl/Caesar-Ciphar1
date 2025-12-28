9441c9e1a4e2c7692e474ff9775a6afc4c01b444
ruchikachaplot25-ctrl <ruchika.chaplot25@gmail.com>
2025-12-28 14:44:29 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,55 +1,18 @@
+auction = {}
 
-import string
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+def highest_bid(bid):
+    find = max(bid.values())
+    for key in bid:
+        if bid[key] == find:
+            print("Winner", key, "With bid", bid[key])
 
-def command():
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-
-    l=''
-    position=0
-    encrypted_text =[]
-    def encrypt():
-        for i in range(len(text)):
-            l = text[i]
-            if l in alphabet:
-                position=alphabet.index(l)
-                #print(f"{position}")
-                new_position = (position + shift) % len(alphabet)
-                encrypted_text.append(alphabet[new_position])
-            elif l in string.ascii_letters or l in string.digits or l in string.whitespace:
-                encrypted_text.append(l)
-        print(''.join(encrypted_text))
-
-    decrypt_text=[]
-    def decrypt():
-        for i in range(len(text)):
-            l = text[i]
-            if l in alphabet:
-                position=alphabet.index(l)
-            #print(f"{position}")
-                new_position = (position - shift) % len(alphabet)
-                decrypt_text.append(alphabet[new_position])
-            elif l in string.ascii_letters or l in string.digits or l in string.whitespace:
-                decrypt_text.append(l)
-        print(''.join(decrypt_text))
-
-    if direction == "encode":
-        encrypt()
-    elif direction == "decode":
-        decrypt()
-    again=input("Wanna run again Y for yencodes N for no").upper()
-    if again=="Y":
-        command()
-    else:
-        print("Bye")
-
-    # 👇 This line actually runs the program
-command()
+morebids = input("Enter 'yes' if more bidders, 'no' if none remaining: ")
+while morebids.lower() == 'yes':
+    user_name = input("Enter the name of the bidder: ")
+    user_price = int(input("Enter the bid price of the user: "))
+    auction[user_name] = user_price
+    morebids = input("Enter 'yes' if more bidders, 'no' if none remaining: ")
+    print("\n" * 50)
 
 
-
-
-
-
+highest_bid(auction)
